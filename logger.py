7bdcdd1029b5eb4722627ecb50ddc5d3e5d6d7c7
nakazono1011 <syousanitiosi1011@yahoo.co.jp>
2024-01-24 00:57:08 +0900

    Add logger module and update main.py

---
+++ logger.py
@@ -0,0 +1,19 @@
+from logging import getLogger, DEBUG, FileHandler, Formatter
+import datetime
+
+now = datetime.datetime.now()
+
+def get_module_logger(
+    module=__name__, filepath=f"./{now.strftime('%Y%m%d')}.log"
+):
+    logger = getLogger(module)
+    logger.setLevel(DEBUG)
+    handler = FileHandler(filepath)
+    handler.setLevel(DEBUG)
+    formatter = Formatter(
+        "%(levelname)s  %(asctime)s  [%(name)s - %(funcName)s] %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import time
 import re
 import requests
 import datetime
+from logger import get_module_logger
+
+logger = get_module_logger(__name__)
 
 def _load_driver():
         """
@@ -14,7 +17,6 @@ def _load_driver():
         """
         options = webdriver.chrome.options.Options()
         options.add_argument('--headless')  # headlessモードを有効にする
-
         chrome_service = fs.Service()
         driver = webdriver.Chrome(service=chrome_service, options=options)
         driver.implicitly_wait(15)
@@ -43,7 +45,7 @@ def extract_floor_number(text):
 
 def extract_specific_layout_type(text):
     # 特定の間取りパターンに一致するかチェック
-    pattern = r'1DK\+S|1LDK|2DK|2LDK'
+    pattern = r'1DK\+S|1LDK|2DK|2LDK|3LDK'
     match = re.search(pattern, text)
     if match:
         # マッチした間取りタイプを返す
@@ -56,36 +58,36 @@ def main():
     driver = None
     while True:
         try:
-                driver = _load_driver()
-                driver.get("https://www.kousha-chintai.com/search/list.php?ot=_b&kw=%E3%83%95%E3%83%AD%E3%83%BC%E3%83%AB%E6%A8%AA%E6%B5%9C%E4%B8%89%E3%83%84%E6%B2%A2")
-                driver.find_element(By.CLASS_NAME, "accordion-btn").click()
-                time.sleep(2)
-                vacancies = driver.find_elements(By.CLASS_NAME, "vacancy_box")
+            driver = _load_driver()
+            driver.get("https://www.kousha-chintai.com/search/list.php?ot=_b&kw=%E3%83%95%E3%83%AD%E3%83%BC%E3%83%AB%E5%B1%B1%E7%94%B0%E7%94%BA+%E3%83%95%E3%83%AD%E3%83%BC%E3%83%AB%E6%A8%AA%E6%B5%9C")
+            accordions = driver.find_elements(By.CSS_SELECTOR, ".title_green.accordion-btn")
+            for accordion in accordions:
+                accordion.click()
+                time.sleep(1)
 
-                for vacancy in vacancies:
-                        title = vacancy.find_element(By.CLASS_NAME, "vacancy_title").text
-                        madori_text = vacancy.find_elements(By.TAG_NAME, "dd")[2].text
+            time.sleep(2)
+            vacancies = driver.find_elements(By.CLASS_NAME, "vacancy_box")
 
-                        floor = extract_floor_number(title)
-                        madori = extract_specific_layout_type(madori_text)
+            for vacancy in vacancies:
+                title = vacancy.find_element(By.CLASS_NAME, "vacancy_title").text
+                madori_text = vacancy.find_elements(By.TAG_NAME, "dd")[2].text
 
-                        # if floor >= 2 and madori in ["2DK", "2LDK"]:
-                        if madori in ["2LDK"] or (floor >= 2 and madori in ["2DK", "2LDK"]):
-                                send_text = f"""
-                                [物件名] {title}
-                                [階数] {floor}階
-                                [間取り] {madori}
-                                """
-                                send_line_notification(send_text)
+                floor = extract_floor_number(title)
+                madori = extract_specific_layout_type(madori_text)
 
-                driver.quit()
-                print("[処理結果]: OK [処理時刻]", datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-                time.sleep(60)
+                # 2LDK か 2階以上の2DKなら通知する
+                if madori in ["2LDK"] or (floor >= 2 and madori in ["2DK"]):
+                    send_text = f"[物件名] {title} \n[階数] {floor}階 \n[間取り] {madori}"
+                    send_line_notification(send_text)
+
+            driver.quit()
+            logger.info("[処理結果] OK")
+            time.sleep(60)
         except Exception as e:
-                print(e)
-                send_line_notification("エラーが発生中！")
-                if driver:
-                        driver.quit()
+            logger.error(f"[処理結果] NG")
+            send_line_notification("エラーが発生中！")
+            if driver:
+                driver.quit()
 
 if __name__ == "__main__":
       main()
